a59f4e1ca4b68f1e0b9c93bf5bd8b50a5046a5bf
Nozugo <msatriokeren@gmail.com>
2023-10-30 23:45:22 +0700

    Add db folder

---
+++ db/celengan.sql
@@ -0,0 +1,114 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 30, 2023 at 04:43 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `celengan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transac`
+--
+
+CREATE TABLE `transac` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `nominal` decimal(15,2) NOT NULL,
+  `description` text NOT NULL,
+  `category` enum('Makanan','Minuman','Transportasi','Belanja') NOT NULL,
+  `payment_type` enum('Cash','Transfer') NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `transac`
+--
+
+INSERT INTO `transac` (`id`, `user_id`, `nominal`, `description`, `category`, `payment_type`, `date`) VALUES
+(1, 1, '100000.00', 'membeli sarapan', 'Makanan', 'Cash', '2023-10-30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `last_edited` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `remember_me` enum('True','False') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `last_edited`, `remember_me`) VALUES
+(1, 'Revo Rahmat', '1234', '2023-10-30 16:33:25', 'True');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transac`
+--
+ALTER TABLE `transac`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_transac` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transac`
+--
+ALTER TABLE `transac`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transac`
+--
+ALTER TABLE `transac`
+  ADD CONSTRAINT `FK_transac` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
