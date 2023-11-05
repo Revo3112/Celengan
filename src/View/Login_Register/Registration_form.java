5a36a1de7feb7b2db0eb48f79f89fe15fbb0244d
Revo3112 <revorahmat@gmail.com>
2023-11-05 21:13:45 +0700

    Add registration form with username, password, and reinput password fields

--- src/View/Login_Register/Registration_form.java
+++ src/View/Login_Register/Registration_form.java
@@ -3,12 +3,15 @@ package View.Login_Register;
 import Controller.SceneController;
 // import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.geometry.*;
 
@@ -37,15 +40,45 @@ public class Registration_form {
         text.setText("Please Register");
         text.setFont(Font.font("Verdana", 20));
         text.setFill(Color.BLACK);
+        text.setTranslateY(-40);
 
         // Tambahkan button ke dalam scene
-        Button button1 = new Button("Register");
+        Button but_regis = new Button("Register");
+        but_regis.setTranslateY(120);
+
+        // Textfield username
+        TextField fieldUsername = new TextField();
+        fieldUsername.setMaxWidth(200);
+        fieldUsername.setTranslateX(90);
+        // Textfield password
+        TextField fieldPassword = new TextField();
+        fieldPassword.setMaxWidth(200);
+        fieldPassword.setTranslateX(90);
+        fieldPassword.setTranslateY(40);
+        // Textfield reinput password
+        TextField fieldReinputPassword = new TextField();
+        fieldReinputPassword.setMaxWidth(200);
+        fieldReinputPassword.setTranslateX(90);
+        fieldReinputPassword.setTranslateY(80);
+
+        // Label username
+        Label labelUsername = new Label("Username:");
+        labelUsername.setTranslateX(-50);
+        // Label password
+        Label labelPassword = new Label("Password:");
+        labelPassword.setTranslateX(-50);
+        labelPassword.setTranslateY(40);
+        // Label reinput password
+        Label labelReinputPassword = new Label("Reinput Password:");
+        labelReinputPassword.setTranslateX(-70);
+        labelReinputPassword.setTranslateY(80);
 
         // Scene
-        VBox root = new VBox();
-        root.getChildren().add(text);
-        root.getChildren().addAll(button1);
-        root.setAlignment(Pos.CENTER);
+        StackPane root = new StackPane();
+        StackPane.setMargin(root, new Insets(20, 0, 0, 0));
+        root.getChildren().addAll(text, but_regis, labelUsername, fieldUsername, labelPassword, fieldPassword,
+                fieldReinputPassword, labelReinputPassword);
+
         Scene scene = new Scene(root, Color.gray(0.2));
         scene.setRoot(root);
 
@@ -53,10 +86,9 @@ public class Registration_form {
         stage.setScene(scene);
         // stage.setFullScreen(true);
         stage.setTitle("Register");
-        stage.setScene(scene);
         stage.show();
 
-        button1.setOnAction(e -> {
+        but_regis.setOnAction(e -> {
             // stage and scene
             SceneController sceneController = new SceneController(stage);
             sceneController.switchToLogin();
