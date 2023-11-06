4f2f6e8eaed4e62c57934193b61c0e268a7222dc
whosejohn <tuturumud@gmail.com>
2023-11-06 13:35:52 +0700

    Splash Screen Folder Changed!

--- src/View/Splash_Screen/SplashScreen.java
+++ src/View/Splash_Screen/SplashScreen.java
@@ -1,20 +1,35 @@
 package View.Splash_Screen;
 
+import java.io.IOException;
+
 /* MAIN */
 import javafx.application.Application;
-import javafx.scene.layout.StackPane;
 import javafx.scene.Scene;
+import javafx.scene.Parent;
+
+/* FXML LOADER */
+import javafx.fxml.FXMLLoader;
 
 /* STAGE */
 import javafx.stage.Stage;
 
 public class SplashScreen {
     Stage stage;
+    Parent root;
 
     public SplashScreen(Stage stage) {
         this.stage = stage;
     }
 
     public void start() {
+        try {
+            root = FXMLLoader.load(getClass().getResource("SplashScreen.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(root);
+
+        this.stage.setScene(scene);
+        this.stage.show();
     }
 }
