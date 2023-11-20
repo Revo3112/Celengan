87d220c9dcafc20f6653397cc778f7fc1bc61b1a
Nozugo <msatriokeren@gmail.com>
2023-11-20 15:31:24 +0700

    Add comments to DBConnection.java

--- src/Utils/DBConnection.java
+++ src/Utils/DBConnection.java
@@ -2,26 +2,28 @@ package Utils;
 import java.sql.*;
 
 public class DBConnection {
-    private Connection connection;
-    private static DBConnection dbc; // dbc = database connection
+    private Connection connection; // Deklarasi variabel connection
+    private static DBConnection dbc; // Deklarasi variabel dbc
+                                    // dbc = database connection
 
     public DBConnection() {
         try {
+            // Inisialisasi field connection dengan method getConnection agar mendapat koneksi ke database
             this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/celengan", "root", "");
-        } catch (SQLException e) {
-            System.out.println("Connection failed: " + e.getMessage());
+        } catch (SQLException e) { // Menangkap error SQLException
+            System.out.println("Connection failed: " + e.getMessage()); // Menampilkan error SQLException
         }
     }
 
     public static DBConnection getDatabaseConnection() {
-        if (dbc == null) {
-            dbc = new DBConnection();
+        if (dbc == null) { // Jika field dbc bernilai null, maka:
+            dbc = new DBConnection(); // Instansiasi field dbc dengan class DBConnection
         }
-        return dbc;
+        return dbc; // Mengembalikan nilai dari field dbc
     }
 
     public Connection getConnection() {
-        return this.connection;
+        return this.connection; // Mengembalikan nilai dari field connection
     }
 
 }
