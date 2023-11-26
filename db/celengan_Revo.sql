1af1263ee064e8e67c11c0ae41208973eb6af768
Revo3112 <revorahmat@gmail.com>
2023-11-27 05:21:28 +0700

    Add database schema and tables

---
+++ db/celengan_Revo.sql
@@ -0,0 +1,207 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 26, 2023 at 10:20 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `celengan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `saldo`
+--
+
+CREATE TABLE `saldo` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `balance` decimal(15,2) NOT NULL DEFAULT '0.00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transac`
+--
+
+CREATE TABLE `transac` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `nominal` decimal(15,2) NOT NULL,
+  `keterangan` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `kategori_id` int NOT NULL,
+  `tipe_pembayaran` enum('Cash','Transfer') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `date` date NOT NULL,
+  `tipe_transaksi` enum('pemasukan','pengeluaran') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transac_kategori`
+--
+
+CREATE TABLE `transac_kategori` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `tipe` enum('pemasukan','pengeluaran') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `transac_kategori`
+--
+
+INSERT INTO `transac_kategori` (`id`, `name`, `tipe`) VALUES
+(1, 'Makanan', 'pengeluaran'),
+(2, 'Minuman', 'pengeluaran'),
+(3, 'Transportasi', 'pengeluaran'),
+(4, 'Pajak', 'pengeluaran'),
+(5, 'Pulsa', 'pengeluaran'),
+(6, 'Tagihan', 'pengeluaran'),
+(7, 'Gaji', 'pemasukan'),
+(8, 'Investasi', 'pemasukan'),
+(9, 'Penjualan', 'pemasukan'),
+(10, 'Deposito', 'pemasukan'),
+(11, 'Penyewaan', 'pemasukan'),
+(12, 'Tabungan', 'pemasukan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `last_edited` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `remember_me` tinyint(1) DEFAULT NULL,
+  `hash` varchar(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_kategori`
+--
+
+CREATE TABLE `user_kategori` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `tipe` enum('pemasukan','pengeluaran') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `saldo`
+--
+ALTER TABLE `saldo`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_unique` (`user_id`);
+
+--
+-- Indexes for table `transac`
+--
+ALTER TABLE `transac`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_transac` (`user_id`);
+
+--
+-- Indexes for table `transac_kategori`
+--
+ALTER TABLE `transac_kategori`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_kategori`
+--
+ALTER TABLE `user_kategori`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_kategori` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `saldo`
+--
+ALTER TABLE `saldo`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transac`
+--
+ALTER TABLE `transac`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `transac_kategori`
+--
+ALTER TABLE `transac_kategori`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user_kategori`
+--
+ALTER TABLE `user_kategori`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `saldo`
+--
+ALTER TABLE `saldo`
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `transac`
+--
+ALTER TABLE `transac`
+  ADD CONSTRAINT `FK_transac` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `user_kategori`
+--
+ALTER TABLE `user_kategori`
+  ADD CONSTRAINT `fk_user_kategori` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
